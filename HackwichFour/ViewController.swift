578d3381d576fc6791e50aff0fe40039e40a28cd
CM Student <cmstudent@UHWO-ACM-C-04.local>
2019-03-28 10:53:52 -1000

    Hackwich #6

--- HackwichFour/ViewController.swift
+++ HackwichFour/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
   
     var myFriends = ["Ryan", "Nicole", "Sara", "Tyler"]
 
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+    }
+    
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return myFriends.count
@@ -22,6 +26,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+    
         //Code Block
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
         let text = myFriends[indexPath.row]
@@ -30,10 +36,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
 
-
+    var restaurantImageData = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //part 7
+        let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        
+        //set restaurantImageDate to the dictionary  object for key 
+        restaurantImageData = dict!.object(forKey:"restaurantImages") as! [String]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -41,7 +54,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //code statements we need to write in order to pass the data from the pList through the Segue:
+        //create an if statement that checks the identifier of the Segue, in this case we want to make sure the Segue is “mySegue”
+        if segue.identifier == "mySegue"
+        {
+            //create an if statement that checks the identifier of the Segue, in this case we want to make sure the Segue is “mySegue”
+            let s1 = segue.destination as! detailViewController
+            //create a variable that holds the index of the cell that's tabbed
+            let imageIndex = tableView.indexPathForSelectedRow?.row
+            
+            s1.imagePass = restaurantImageData[imageIndex!]
+        }
+    }
 
 }
+
 
